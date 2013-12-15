3b0406f8947622edd19cd1453f23a87b445e2e81
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-14 22:12:04 -0500

    Added URL model.

--- go/go/models.py
+++ go/go/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class URL( models.Model ):
+    owner = models.OneToOneField( User )
+    date_created = models.DateTimeField( default=timezone.now() )
+
+    target = models.CharField( max_length = 1000 )
+    short = models.CharField( max_length = 50 )
+    clicks = models.IntegerField( default = 0 )
+
