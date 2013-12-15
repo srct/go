e816f318f38a8a191efc4cd55c8128553c757a73
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-14 22:43:05 -0500

    Added basic views.

--- go/go/views.py
+++ go/go/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from go.models import URL
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+# Homepage view.
+def index(request):
+    return render(request, 'index.html', {
+
+    },
+    )
+
+# About page, static.
+def about(request):
+    return render(request, 'about.html', {
+
+    },
+    )
