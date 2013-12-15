23098ff9503668d054a44d8066371ec30b88ae00
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-14 22:43:22 -0500

    Added about, homepage urls.

--- go/settings/urls.py
+++ go/settings/urls.py
@@ -4,9 +4,22 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('go.views',
-    # Examples:
-    # url(r'^$', 'go.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+    # / - Homepage url.
+    url(r'^$', 'index', name = 'homepage'),
+
+    # /about - About page.
+    url(r'^about/?$', 'about', name = 'about'),
+
+    # /admin - Administrator interface.
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.contrib.auth.views',
+
+    # Auth pages
+    url(r'^login$', 'login', {'template_name' : 'login.html'},
+        name='website_login'),
+    url(r'^logout$', 'logout', {'next_page' : '/'},
+        name='website_logout'),
 )
