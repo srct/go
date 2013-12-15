0f9bd4964f0f8684810e9b5ab7164d1a0de33699
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-15 17:38:39 -0500

    Added delete url, renamed homepage to index.

--- go/settings/urls.py
+++ go/settings/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('go.views',
 
     # / - Homepage url.
-    url(r'^$', 'index', name = 'homepage'),
+    url(r'^$', 'index', name = 'index'),
 
     # /about - About page.
     url(r'^about/?$', 'about', name = 'about'),
@@ -16,6 +16,9 @@ urlpatterns = patterns('go.views',
 
     # /my - My-Links page, view and review links.
     url(r'^my/?$', 'my_links', name = 'my_links'),
+
+    # /delete - Delete a link, no content display.
+    url(r'^delete/(?P<short>\w+)$', 'delete', name = 'delete'),
 
     # /admin - Administrator interface.
     url(r'^admin/', include(admin.site.urls)),
