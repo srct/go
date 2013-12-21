5de09ff4e0d58bd698f564764c175994ab9b3825
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-21 00:30:40 -0500

    Removed old dead code.

--- static/go.js
+++
@@ -1,8 +0,0 @@
-function expand( id ) { 
-  var box = document.getElementById( id );
-  if( box.style.display=="none" ) { 
-    box.style.display="block";
-  } else {
-    box.style.display="none";
-  }
-}
