0e0427d317852342fb827fbb21ff39931491090c
Daniel W Bond <dbond2@gmu.edu>
2015-12-17 16:20:32 -0500

    basic custom, far more useful admin interface-- to be extended or updated with changes to models

--- go/go/admin.py
+++ go/go/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from go.models import URL, RegisteredUser
 
 
-admin.site.register(URL)
-admin.site.register(RegisteredUser)
+class URLAdmin(admin.ModelAdmin):
+    list_display = ("target", "short", "owner", "clicks", "date_created", "expires")
+
+
+class RegisteredUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "full_name", "approved")
+
+admin.site.register(URL, URLAdmin)
+admin.site.register(RegisteredUser, RegisteredUserAdmin)
