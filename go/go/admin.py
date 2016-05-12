4780f0f20c34f70789ed5c78b440c096d9b929ca
David Haynes <dhaynes207@gmail.com>
2016-05-12 15:04:40 -0400

    Associate RegisteredUser with User
    - a registered user is displayed inline with user since it is only a wrapper class
    - comments added as well for added usability

--- go/go/admin.py
+++ go/go/admin.py
@@ -1,15 +1,24 @@
 # Django Imports
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 # App Imports
 from go.models import URL, RegisteredUser
 
-
+# Define what attributes display in the URL Admin
 class URLAdmin(admin.ModelAdmin):
     list_display = ("target", "short", "owner", "clicks", "date_created", "expires")
 
+# Define an inline admin descriptor for User model
+class RegisteredUserInline(admin.StackedInline):
+  model = RegisteredUser
+  can_delete = False
 
-class RegisteredUserAdmin(admin.ModelAdmin):
-    list_display = ("full_name", "organization", "approved")
+# Define a new User admin
+class UserAdmin(UserAdmin):
+  inlines = (RegisteredUserInline, )
 
+# Register URLAdmin and modify User to use new UserAdmin
 admin.site.register(URL, URLAdmin)
-admin.site.register(RegisteredUser, RegisteredUserAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
