6ec0656e4627230e1d0a31e8de0dbb8c402c1837
Robert Hitt <rhitt@gmu.edu>
2016-10-15 01:11:22 -0400

    Made the Copy button tooltip prettier.

--- go/static/js/copy.js
+++ go/static/js/copy.js
@@ -2,26 +2,20 @@
  * Script for copying text and displaying a tooltip.
  */
 
-$('#btn').tooltip({
-    trigger: 'click',
+var clipboard = new Clipboard('#copy-button');
+
+$('#copy-button').tooltip({
+    trigger: 'hover',
     placement: 'bottom'
 });
 
-function setTooltip(button, message) {
-    $(button).tooltip('hide').attr('data-original-title', message)
-        .tooltip('show');
-}
-
-function hideTooltip(button) {
-    window.setTimeout(function() {
-        $(button).tooltip('hide');
-    }, 700);
-}
-
-var clipboard = new Clipboard('#btn');
+$('#copy-button').on('hidden.bs.tooltip', function () {
+    $('#copy-button').attr('data-original-title', 'Copy to Clipboard');
+});
 
 clipboard.on('success', function(e) {
     e.clearSelection();
-    setTooltip(e.trigger, 'Copied!');
-    hideTooltip(e.trigger);
+    $('#copy-button').attr('data-original-title', 'Copied!').tooltip('show');
 });
+
+
