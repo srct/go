23fe1e77742825bb3ebe6e0c7717377801f9cd2f
David Haynes <dhaynes207@gmail.com>
2017-01-03 11:16:20 -0800

    Add RegisteredUserTest model
    - and a sample test

--- go/go/test_models.py
+++ go/go/test_models.py
@@ -7,10 +7,18 @@ from go.models import URL, RegisteredUser
 """
     Test cases for the URL Model
 """
-class URLTestCase(TestCase):
+class URLTest(TestCase):
 
     """
         Default test case, does not actually test anything
     """
     def test_Django_Test(self):
         self.assertEqual("Hello World!", "Hello World!")
+
+"""
+    Test cases for the RegisteredUser Model
+"""
+class RegisteredUserTest(TestCase):
+
+    def test_Django_Test(self):
+        self.assertEqual(1+1, 2)
