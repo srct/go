74f906332013d3a040114f587c948012005a2461
Zosman <zach.osman@gmail.com>
2017-02-02 15:19:19 -0500

    trying to get an error message when nothing shows up. fuck it. probably gonna open an issue for that and make it bite sized cuz i dont wanna. :dhaynes: :heart:

--- go/static/js/useradmin.js
+++ go/static/js/useradmin.js
@@ -1,16 +1,17 @@
 function myFunction() {
   var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[2];
+  input = document.getElementById("myInput"); // the var that holdes input from text box
+  filter = input.value.toUpperCase(); //makes the input all uppercase for some fucking reason
+  table = document.getElementById("myTable"); // the table at the moment this is called
+  tr = table.getElementsByTagName("tr"); //the table rows
+  for (i = 1; i < tr.length; i++) { // loops through the table rows
+    td = tr[i].getElementsByTagName("td")[2]; //sets td to the element, gets changed evrey loop itteration
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
+        // tr[1].style.display = "No results match your search! ¯\\_(ツ)_/¯"
       }
     }
   }
