0f3b2d4318a55150baf135b86c1b67e396c3fde2
David Haynes <dhaynes207@gmail.com>
2017-02-04 14:50:32 -0500

    Additional comments added to test_models
    - list out what we need to do

--- go/go/test_models.py
+++ go/go/test_models.py
@@ -9,18 +9,16 @@ from django.contrib.auth.models import User
 from go.models import URL, RegisteredUser
 
 """
-    Test cases for the URL Model
-"""
-class URLTest(TestCase):
-
-    """
-        Default test case, does not actually test anything
-    """
-    def test_Django_Test(self):
-        self.assertEqual("Hello World!", "Hello World!")
-
-"""
     Test cases for the RegisteredUser Model
+
+    - check if RegisteredUsers are actually made
+    - check approval and registration status flipping
+    - check blocking
+    - check printing
+    - add in description
+    - check organization field
+    - check full name field
+    - check print(RegisteredUser)
 """
 class RegisteredUserTest(TestCase):
 
@@ -31,3 +29,22 @@ class RegisteredUserTest(TestCase):
         getUser = User.objects.get(username='dhaynes')
         getRegisteredUser = RegisteredUser.objects.get(user=getUser)
         self.assertTrue(getRegisteredUser)
+
+    def test_checkPrint(self):
+        # expected = '<Registered User: %s - Approval Status: %s>' % (self.user, self.approved)
+
+"""
+    Test cases for the URL Model
+
+    - check if URL's are actually created
+    - modify clicks (social, qr, normal)
+    - check expiration date creation
+    - check print function
+"""
+class URLTest(TestCase):
+
+    """
+        Default test case, does not actually test anything
+    """
+    def test_Django_Test(self):
+        self.assertEqual("Hello World!", "Hello World!")
