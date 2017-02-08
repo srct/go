fe4a55ef509936b14b7f3c1d53ab443a99183211
David Haynes <dhaynes207@gmail.com>
2017-02-07 19:32:27 -0500

    First pass of test_urls complete
    - variable names changed

--- go/settings/test_urls.py
+++ go/settings/test_urls.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 # Django Imports
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 # App Imports
 from .urls import *
@@ -18,91 +18,91 @@ class UrlsTest(TestCase):
     """
         / - Homepage url.
     """
-    def test_index(self):
+    def test_index_reverse(self):
         url = reverse('index')
         self.assertEqual(url, '/')
 
     """
         /view/<short> - View URL data.
     """
-    def test_view_chars(self):
+    def test_view_reverse_chars(self):
         url = reverse('view', args=['dhaynes'])
         self.assertEqual(url, '/view/dhaynes')
 
     """
         /view/<short> - View URL data.
     """
-    def test_view_ints(self):
+    def test_view_reverse_ints(self):
         url = reverse('view', args=['123456789'])
         self.assertEqual(url, '/view/123456789')
 
     """
         /view/<short> - View URL data.
     """
-    def test_view_chars_ints(self):
+    def test_view_reverse_chars_ints(self):
         url = reverse('view', args=['dhaynes123'])
         self.assertEqual(url, '/view/dhaynes123')
 
     """
         /view/<short> - View URL data.
     """
-    def test_view_full_slug(self):
+    def test_view_reverse_full_slug(self):
         url = reverse('view', args=['dhaynes123_-'])
         self.assertEqual(url, '/view/dhaynes123_-')
 
     """
         /about - About page.
     """
-    def test_about(self):
+    def test_about_reverse(self):
         url = reverse('about')
         self.assertEqual(url, '/about')
 
     """
         /signup - Signup page for access.
     """
-    def test_signup(self):
+    def test_signup_reverse(self):
         url = reverse('signup')
         self.assertEqual(url, '/signup')
 
     """
         /myLinks - My-Links page, view and review links.
     """
-    def test_my_links(self):
+    def test_my_links_reverse(self):
         url = reverse('my_links')
         self.assertEqual(url, '/myLinks')
 
     """
         /delete/<short> - Delete a link, no content display.
     """
-    def test_delete_chars(self):
+    def test_delete_reverse_chars(self):
         url = reverse('delete', args=['dhaynes'])
         self.assertEqual(url, '/delete/dhaynes')
 
     """
         /delete/<short> - Delete a link, no content display.
     """
-    def test_delete_ints(self):
+    def test_delete_reverse_ints(self):
         url = reverse('delete', args=['123456789'])
         self.assertEqual(url, '/delete/123456789')
 
     """
         /delete/<short> - Delete a link, no content display.
     """
-    def test_delete_chars_ints(self):
+    def test_delete_reverse_chars_ints(self):
         url = reverse('delete', args=['dhaynes123'])
         self.assertEqual(url, '/delete/dhaynes123')
 
     """
         /delete/<short> - Delete a link, no content display.
     """
-    def test_delete_full_slug(self):
+    def test_delete_reverse_full_slug(self):
         url = reverse('delete', args=['dhaynes123_-'])
         self.assertEqual(url, '/delete/dhaynes123_-')
 
     """
         /registered - registration complete page
     """
-    def test_registered(self):
+    def test_registered_reverse(self):
         url = reverse('registered')
         self.assertEqual(url, '/registered')
 
