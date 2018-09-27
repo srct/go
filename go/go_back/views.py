9835afe5932e8e8257ae1fcb9802397724980115
David Haynes <dhaynes3@gmu.edu>
2018-09-27 19:56:09 -0400

    Add /auth/token API endpoint
    - Returns the current user's API token if there is a authenticated
    session already established

--- go/go_back/views.py
+++ go/go_back/views.py
@@ -4,55 +4,58 @@ go/views.py
 The functions that handle a request to a given URL. Get some data, manipulate
 it, and return a rendered template.
 """
-# Python stdlib imports
-from datetime import timedelta
-
-# Django Imports
-from django.conf import settings
-from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.core.exceptions import PermissionDenied  # ValidationError
-from django.core.mail import EmailMessage, send_mail
-from django.http import HttpResponseServerError  # Http404
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
-from django.utils import timezone
-# Other imports
-from ratelimit.decorators import ratelimit
-
-# App Imports
-from .forms import EditForm, SignupForm, URLForm
-from .models import URL, RegisteredUser
-
-
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from .serializers import URLSerializer
+from .models import URL
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+
 
 class URLPermission(permissions.BasePermission):
+    """Custom permission check on URL model operations."""
+
     message = "You do not have the necessary approvals to perform that action."
+
     def has_permission(self, request, view):
         return request.user.registereduser.approved or request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user.registereduser or request.user.is_staff
 
+
 class URLViewSet(viewsets.ModelViewSet):
     """
     API endpoint that handles creation/read/update/deletion of URL objects.
     """
-    authentication_classes = (TokenAuthentication, )
+
+    authentication_classes = (TokenAuthentication,)
     serializer_class = URLSerializer
-    permission_classes = (URLPermission,)
-    lookup_field = 'short'
+    permission_classes = (URLPermission, IsAuthenticated)
+    lookup_field = "short"
 
     def get_queryset(self):
         if not self.request.user.is_staff:
             return URL.objects.filter(owner=self.request.user.registereduser)
-        else:
-            return URL.objects.all()
+        return URL.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user.registereduser)
+
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+
+class CustomAuthToken(ObtainAuthToken):
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        token, created = Token.objects.get_or_create(user=request.user)
+        return Response({"token": token.key})
+
