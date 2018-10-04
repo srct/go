35936f4862fdb3f2a5a188ced88914f57b468ee2
Zach Osman <zosman@gmu.edu>
2018-10-04 15:20:25 -0400

    This DOES work

--- go/go_ahead/src/Components/Pages/HomePage.js
+++ go/go_ahead/src/Components/Pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default class HomePage extends Component {
+    state = {};
     render() {
         return (
             <div>
--- go/go_ahead/src/Components/Pages/index.js
+++ go/go_ahead/src/Components/Pages/index.js
@@ -1,3 +1,3 @@
-import HomePage from './HomePage.jsx';
+import HomePage from './HomePage';
 
 export { HomePage };
