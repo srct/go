7d519f798571d81b96031fbd1a64e33b741de81a
Paul Otto <potto@gmu.edu>
2018-10-14 07:56:15 -0400

    Added about page component

---
+++ go/go_ahead/src/Components/Pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class AboutPage extends React.Component {
+    state = {};
+    render() {
+        return (
+            <div>
+                <p>About page</p>
+            </div>
+        );
+    }
+}
--- go/go_ahead/src/index.js
+++ go/go_ahead/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter, Route, Link } from 'react-router-dom';
 
 import { HomePage } from './Components';
+import { ABoutPage } from './Components';
 
 const Dhaynes = () => <p>dhaynes</p>;
 
@@ -15,9 +16,13 @@ const App = () => (
             <li>
                 <Link to="/dhaynes">dhaynes</Link>
             </li>
-        </ul>
+            <li>
+                <Link to="/About">About</Link>
+            </li>
+         </ul>
         <Route path="/" exact component={HomePage} />
         <Route path="/dhaynes" component={Dhaynes} />
+        <Route path="/About" component={AboutPage} />
     </div>
 );
 
