7918fe45757bad379d04c52e25953f361191d97e
Zach Osman <zosman@gmu.edu>
2018-10-16 18:43:17 -0400

    Small style changes

--- go/go_ahead/src/Components/Pages/DhaynesPage.js
+++ go/go_ahead/src/Components/Pages/DhaynesPage.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { PageTemplate } from 'Components';
 
 export default class DhaynesPage extends React.Component {
-    state = {};
     render() {
         return (
             <PageTemplate page={'dhaynes'}>
--- go/go_ahead/src/Components/Pages/HomePage.js
+++ go/go_ahead/src/Components/Pages/HomePage.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { PageTemplate } from 'Components';
 
 export default class HomePage extends React.Component {
-    state = {};
     render() {
         return (
             <PageTemplate page={'home'}>
