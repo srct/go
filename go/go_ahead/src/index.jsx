c6226e5903bd45c17d975672561347010668ebcf
David Haynes <dhaynes3@gmu.edu>
2018-10-18 12:27:50 -0400

    Confirm auth flow works with API
    - todo in future issue, demo workflow of grabbing API token and making a API call
    
    Closes #187

--- go/go_ahead/src/index.jsx
+++ go/go_ahead/src/index.jsx
@@ -4,12 +4,10 @@ import "masonstrap/build/js/masonstrap.min.js";
 import React from "react";
 import ReactDOM from "react-dom";
 import AuthButton from "./AuthButton.jsx";
-import Golinkslist from "./GolinksList.jsx";
 
 ReactDOM.render(
   <div>
     <AuthButton />
-    <Golinkslist />
   </div>,
   document.getElementById("root")
 );
