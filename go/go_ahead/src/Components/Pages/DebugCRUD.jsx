699bd15cc3e745410ff833767597a7f52d9ba46f
David Haynes <dhaynes3@gmu.edu>
2018-12-29 14:27:03 -0500

    Add an example formik + yup form
    - super duper nice

--- go/go_ahead/src/Components/Pages/DebugCRUD.jsx
+++ go/go_ahead/src/Components/Pages/DebugCRUD.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!"),
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Required")
+});
 
 const DebugCRUD = () => (
   <div>
     <h1>Debug CRUD Page</h1>
+    <div>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={SignupSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            console.log(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+        render={({ isSubmitting }) => (
+          <Form>
+            <Field type="email" name="email" placeholder="Email" />
+            <ErrorMessage name="email" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      />
+    </div>
   </div>
 );
 
