35506d4960652478a7b55637956a3ffd33ce1374
David Haynes <dhaynes3@gmu.edu>
2019-01-02 12:52:52 -0500

    Really sketch implementation of submitting go links
    - to be abstracted and prettified

--- go/go_ahead/src/Components/Pages/DebugCRUD.jsx
+++ go/go_ahead/src/Components/Pages/DebugCRUD.jsx
@@ -3,19 +3,38 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { PageTemplate, DebugRead } from "Components";
 
-const SignupSchema = Yup.object().shape({
-  password: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!"),
-  email: Yup.string()
-    .email("Invalid email")
+const DebugCreate = Yup.object().shape({
+  destination: Yup.string()
+    .url()
+    .max(1000, "Too Long!"),
+  short: Yup.string()
     .required("Required")
+    .max(20, "Too Long!")
+  // expires: Yup.date()
+  //   .nullable()
+  //   .min(new Date(new Date().getTime() + 24 * 60 * 60 * 1000))
 });
 
 class DebugCRUD extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != "") {
+      var cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
   }
 
   render() {
@@ -26,20 +45,29 @@ class DebugCRUD extends React.Component {
 
           <h3>Create</h3>
           <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={SignupSchema}
+            initialValues={{ destination: "", short: "", expires: null }}
+            validationSchema={DebugCreate}
             onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                console.log(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
+              fetch("/api/golinks/", {
+                method: "post",
+                headers: {
+                  "Content-Type": "application/json",
+                  "X-CSRFToken": this.getCookie("csrftoken")
+                },
+                body: JSON.stringify(values)
+              }).then(response => console.log(response));
             }}
             render={({ isSubmitting }) => (
               <Form>
-                <Field type="email" name="email" placeholder="Email" />
-                <ErrorMessage name="email" component="div" />
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" />
+                <Field
+                  name="destination"
+                  placeholder="https://longwebsitelink.com"
+                />{" "}
+                <ErrorMessage name="destination" component="div" />
+                <Field name="short" />
+                <ErrorMessage name="short" />
+                <Field type="select" name="expires" />
+                <ErrorMessage name="expires" />
                 <button type="submit" disabled={isSubmitting}>
                   Submit
                 </button>
@@ -51,50 +79,7 @@ class DebugCRUD extends React.Component {
           <DebugRead />
 
           <h3>Update</h3>
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={SignupSchema}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                console.log(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
-            }}
-            render={({ isSubmitting }) => (
-              <Form>
-                <Field type="email" name="email" placeholder="Email" />
-                <ErrorMessage name="email" component="div" />
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" />
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
-            )}
-          />
-
           <h3>Delete</h3>
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={SignupSchema}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                console.log(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
-            }}
-            render={({ isSubmitting }) => (
-              <Form>
-                <Field type="email" name="email" placeholder="Email" />
-                <ErrorMessage name="email" component="div" />
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" />
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
-            )}
-          />
         </div>
       </PageTemplate>
     );
