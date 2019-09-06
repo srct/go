4a1f8960912a0c585cabf200bc204a70532ede76
Zac Wood <zac.wood9@gmail.com>
2019-09-05 20:54:35 -0400

    don't delete the production database

--- startup_prod.sh
+++ startup_prod.sh
@@ -1,5 +1,4 @@
 export GO_SECRET_KEY=$(dd if=/dev/urandom count=100 | tr -dc "A-Za-z0-9" | fold -w 60 | head -n1 2>/dev/null)
-python go/manage.py flush --no-input
 python go/manage.py makemigrations
 python go/manage.py makemigrations go
 python go/manage.py migrate
